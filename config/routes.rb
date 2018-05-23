2048abc80a7165463f068404b13d96a6f0f5f45b
chaudard <donnetonfalzard+github@gmail.com>
2018-05-23 17:42:31 +0200

    correction invalid rating

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,4 @@
 Rails.application.routes.draw do
-  get 'reviews/new'
-  get 'reviews/create'
-  get 'restaurants/index'
-  get 'restaurants/show'
-  get 'restaurants/new'
-  get 'restaurants/create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :restaurants, only: [ :index, :show, :new, :create ] do
     resources :reviews, only: [ :new, :create ]
